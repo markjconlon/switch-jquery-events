065b194f14d3d5a6ad8b94e3e2f97a1bab2e0f69
Mark <markjconlon@gmail.com>
2017-04-29 14:18:46 -0400

    added if/else statement changing .status txt based on the on/off class of this

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ $(function() {
 
     $('body').toggleClass('light');
     $('body').toggleClass('dark');
+
+    if ($(this).hasClass('on')) {
+      $('.status').text('It\'s so bright in here!');
+    } else {
+      $('.status').text('Hey, who turned off the lights?');
+    }
   });
 
 });
