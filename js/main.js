bf3f189aa96b42e1c8bbc53c3e18cf4d0bcc013c
Mark <markjconlon@gmail.com>
2017-04-29 14:11:24 -0400

    added click listener to the button.switch and ttoggles between the on and off class as well the light and dark class

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+$(function() {
+  $('button.switch').on('click', function(){
+    $(this).toggleClass('on');
+    $(this).toggleClass('off');
 
+    $('body').toggleClass('light');
+    $('body').toggleClass('dark');
+  });
 
+});
